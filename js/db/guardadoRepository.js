9bfac089b1ffb3bb84f280f68442746353745e5e
bn1ja89s <bn1ja89s@gmail.com>
2026-05-01 20:15:00 -0500

    casd

---
+++ js/db/guardadoRepository.js
@@ -0,0 +1,21 @@
+import { eliminarDocumento, guardarYSincronizar, obtenerDocumentos } from "../services/backendService.js";
+
+const STORE = "guardado";
+
+export async function listGuardado() {
+  const items = await obtenerDocumentos(STORE);
+  return items.sort((a, b) => String(b.fecha).localeCompare(String(a.fecha)));
+}
+
+export function saveGuardado(item) {
+  return guardarYSincronizar(STORE, item);
+}
+
+export function deleteGuardadoById(id) {
+  return eliminarDocumento(STORE, id);
+}
+
+export async function clearAllGuardado() {
+  const items = await listGuardado();
+  await Promise.all(items.map((item) => eliminarDocumento(STORE, item.id)));
+}
